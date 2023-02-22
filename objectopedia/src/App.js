c7064e8740bd718826a89f45619e7a2a3a1ab8fd
SubodhSingh8543 <subodhsingh8543@gmail.com>
2023-02-23 01:02:52 +0530

    4thcommit

--- objectopedia/src/App.js
+++ objectopedia/src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+// import './App.css';
 import { useState } from 'react';
-import {  } from "module";
 import AdminNavbar from './Admin/Components/adminNavbar';
 import AllRoutes from './Components/AllRoutes';
 
 function App() {
-  const [athenticated,setAuth] = useState(true);
+  const [athenticated,setAuth] = useState(false);
 
   return (
     <div className="App">
